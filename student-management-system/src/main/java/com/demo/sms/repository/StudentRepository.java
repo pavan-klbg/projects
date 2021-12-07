f9a96e0e42b9daeedd742f028f55755bf666d39c
pavan-klbg <allurpavan111@gmail.com>
2021-12-07 15:25:26 +0530

    delete

--- student-management-system/src/main/java/com/demo/sms/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.sms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.sms.entity.Student;
-
-public interface StudentRepository  extends JpaRepository<Student, Long>{
-
-}
