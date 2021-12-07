e08a686b836c66cd5c78cdf2ce3a4507f3d8a4e7
pavan-klbg <allurpavan111@gmail.com>
2021-12-07 22:49:15 +0530

    initial commit

---
+++ student-management-system/src/main/java/com/demo/sms/serviceimpl/ServiceImpl.java
@@ -0,0 +1,27 @@
+
+package com.demo.sms.serviceimpl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.demo.sms.entity.Student;
+import com.demo.sms.repository.StudentRepository;
+import com.demo.sms.service.StudentService;
+
+@Service
+public class ServiceImpl implements StudentService{
+
+	private StudentRepository studentRepository;
+	
+	public ServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+
+	public List<Student> getAllStudents() {
+		return studentRepository.findAll();
+	}
+
+}
