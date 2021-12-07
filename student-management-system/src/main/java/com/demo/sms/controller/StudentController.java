a87f0d4ec5053a509a7f9d7918cfa9fbc5a948b3
pavan-klbg <allurpavan111@gmail.com>
2021-12-08 01:18:35 +0530

    controler update

--- student-management-system/src/main/java/com/demo/sms/controller/StudentController.java
+++ student-management-system/src/main/java/com/demo/sms/controller/StudentController.java
@@ -1,8 +1,22 @@
 package com.demo.sms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.demo.sms.service.StudentService;
 
 @Controller
 public class StudentController {
+private StudentService studentService;
+
+public StudentController(StudentService studentService) {
+	super();
+	this.studentService = studentService;
+}
+public String listStudents( Model model) {
+	model.addAttribute("students", studentService.getAllStudents());
+	return "students";
+	
+}
 
 }
